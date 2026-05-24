f339accce05f1ff06f6b727d7e88a1734555f8eb
Formatier <formatier.com@gmail.com>
2026-05-24 12:25:55 +0700

    fix: hero section

--- app/components/promote/hero/index.vue
+++ app/components/promote/hero/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="vision-padding-x flex justify-between lg:py-48 py-32">
+    <div class="vision-padding-x flex justify-between py-30 lg:py-48 md:py-32">
         <slot></slot>
     </div>
 </template>
