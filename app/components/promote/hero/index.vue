3b3e59a20152c728ad681550b2a385557e2dd527
Formatier <formatier.com@gmail.com>
2026-05-31 01:00:52 +0700

    fix: hiding hero gallery

--- app/components/promote/hero/index.vue
+++ app/components/promote/hero/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="vision-padding-x grid grid-cols-2 gap-6 lg:items-start lg:justify-between 2xl:py-32 xl:py-24 md:py-20 py-18"
+        class="vision-padding-x grid lg:grid-cols-2 gap-6 lg:items-start lg:justify-between 2xl:py-32 xl:py-24 md:py-20 py-18"
     >
         <slot/>
     </div>
