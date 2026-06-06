493554406cd15dc7a1c9bccc9092d2ac2faca84d
formatier <formatier.com@gmail.com>
2026-06-07 00:36:42 +0700

    add: hover effect

--- app/components/promote/section/member/card/image.vue
+++ app/components/promote/section/member/card/image.vue
@@ -7,6 +7,7 @@ const props = withDefaults(defineProps<{ src?: string }>(), {
 <template>
     <div
         class="w-full lg:h-full overflow-hidden absolute bottom-0 -right-14 lg:static lg:flex items-end -z-10 select-none"
+        id="image"
     >
         <NuxtImg :src="props.src" class="aspect-square w-full" />
     </div>
