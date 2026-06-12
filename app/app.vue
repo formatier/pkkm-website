5aa71590c6dfa7885a36471d47fb4f682076c44e
formatier <formatier.com@gmail.com>
2026-06-12 22:16:39 +0700

    temp: disable website

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,27 @@
 <template>
     <NuxtRouteAnnouncer />
     <NuxtLoadingIndicator />
-    <NuxtLayout>
+    <NuxtLayout v-if="false">
         <NuxtPage :transition="{ name: 'page', mode: 'out-in' }"></NuxtPage>
     </NuxtLayout>
+    <div class="flex flex-col w-full h-dvh items-center justify-center">
+        <div class="font-prompt prose-xl">
+            <h1>เว็บไซต์ปิดตัวลงชั่วคราว</h1>
+            <p class="max-w-lg">
+                ด้วยเหตุผลทางการเลือกตั้งสภานักเรียนในวันที่ 15 มิถุนายน 2569
+                ได้ระบุเอาไว้ว่า ห้ามหาเสียงในวันเลือกตั้ง
+                ทางเราจึงปิดตัวเว็บไซต์ลงชั่วคราว
+            </p>
+            <p class="max-w-lg">
+                ทั้งนี้
+                ยังสามารถเข้าถึงเว็บไซต์ได้โดยการคัดลอกโปรเจกต์หรือซอร์สโค้ดและนำมาเปิดใช้งานในสภาพแวดล้อมเครื่อข่ายถ้องถิ่น
+            </p>
+            <p class="max-w-lg">
+                และจะกลับมาเปิดใหม่อีกครั้งในวันที่ 16 มิถุนายน 2569 ด้วยเหตุผล
+                เพื่อนำไปใช้เพื่อการศึกษาการทำงานต่าง ๆ
+            </p>
+        </div>
+    </div>
 </template>
 
 <style>
